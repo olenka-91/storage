c05fdda65af71126390dda4109410018abfbff42
olenka-91 <olenka--91@mail.ru>
2024-05-15 13:54:50 +0300

    Add simple Upload

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/olenka-91/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("It works", st)
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("It works", file)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,17 @@
 package storage
 
+import "github.com/olenka-91/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	newFile, err := file.NewFile(filename, blob)
+	if err != nil {
+		return nil, err
+	}
+	return newFile, err
 }
